b6d6a393755ffc7c9419548a0c33ff4ff4a74817
Idang1410 <Idang1410@gmail.com>
2020-11-13 17:54:01 +0200

    Finished basic implemantation

--- subspo.py
+++ subspo.py
@@ -1,16 +1,36 @@
+from typing import List
 import argparse
-import googletrans
+from googletrans import Translator
 import srt
 
+LANG_DETECTION = "auto"
+"""
+Make one ticket (one object), and translte all in a bulk for one request.
+Many tickets or requests will be seem like bot
+"""
+def translate(lines: List[str], dst_lang: str, origin_lang: str = None):
+    translator = Translator()
+    if not origin_lang: # language detection
+        origin_lang = LANG_DETECTION
 
-def main(subs_file : str, origin_lang: str, dst_lang: str):
+    outputs = translator.translate(lines, dst=dst_lang, src=origin_lang)
+    return outputs
+
+
+def main(subs_file : str,  dst_lang: str, origin_lang: str):
     print(f"{subs_file}, {origin_lang}, {dst_lang}")
     with open(subs_file) as file:
         content = file.read()
 
     subs = list(srt.parse(content))
+    lines_content = []
     for line in subs:
-        line.content = translate(line.content, origin_lang, dst_lang)
+        lines_content.append(line.content)
+    
+    translated = translate(lines_content, dst_lang, origin_lang)
+    for origin, new_content in zip(subs, translated):
+        origin.content = new_content 
+    
 
     with open(f"{subs_file}.out", "w") as file:
         file.write(srt.compose(subs))
@@ -18,7 +38,7 @@ def main(subs_file : str, origin_lang: str, dst_lang: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Auto translate substitles")
     parser.add_argument("file", help="The subs file")
-    parser.add_argument("origin_lang", help="The original substitles langugage")
     parser.add_argument("dst_lang", help="The wanted language for the subs")
+    parser.add_argument("--origin_lang", help="The original substitles langugage", default=LANG_DETECTION)
     args = parser.parse_args()
-    main(args.file, args.origin_lang, args.dst_lang)
+    main(args.file,  args.dst_lang, args.origin_lang)
